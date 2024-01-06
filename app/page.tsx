9cb9fcb0f40cd9fc785b7c39cb8348d377856ac1
Oleg <139042293+olehplay@users.noreply.github.com>
2024-01-06 13:31:37 +0200

    Challenge: Add Custom Google Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import TradeHubLogo from '@/app/ui/tradehub-logo';
 import Link from 'next/link';
 import style from '@/app/ui/home.module.css';
+import { rubik } from './ui/fonts';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col bg-black p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-sky-700 p-4 md:h-52">
-        {/* <TradeHubLogo /> */}
+        <TradeHubLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row ">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-neutral-900 px-6 py-10 md:w-2/5 md:px-20">
           <div className={style.circle} />
-          <p className={`text-5xl text-white md:text-3xl md:leading-normal`}>
+          <p
+            className={`${rubik.className} text-5xl text-white md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to TradeHub!</strong>
           </p>
           <Link
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import { Poppins } from 'next/font/google';
+import { Poppins, Rubik } from 'next/font/google';
 
 export const poppins = Poppins({ weight: ['400'], subsets: ['latin'] });
+export const rubik = Rubik({ weight: ['400'], subsets: ['latin'] });
