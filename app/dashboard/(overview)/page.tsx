6de0e14db68145e3b89fd6e671380abf7049f675
Oleg <139042293+olehplay@users.noreply.github.com>
2024-01-07 11:29:50 +0200

    Specific Parts Loaders

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { poppins } from '@/app/ui/fonts';
-import {
-  fetchIncome,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { IncomeChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const income = await fetchIncome();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfSellers,
@@ -36,7 +33,9 @@ export default async function Page() {
         <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <IncomeChart income={income} />
+        <Suspense fallback={<IncomeChartSkeleton />}>
+          <IncomeChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
